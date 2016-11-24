99bdbf46ee87fbb81840b86b1f35c517e8dd8eb7
henrickd <henrickdavis@yahoo.ca>
2016-11-23 21:55:50 -0500

    BlockSearch and OdometryCorrection changes
    BlockSearch: now checks a 90 degree cone when traveled to object
    location
    OdometryCorrection: fixed what I believe was a fatal flaw

--- src/dpm/odometry/OdometryCorrection.java
+++ src/dpm/odometry/OdometryCorrection.java
@@ -45,51 +45,74 @@ public class OdometryCorrection extends Thread implements DPMConstants{
 			sensor.fetchSample(sensorData, 0);
 			
 			if(sensorData[0] < LIGHT_THRESHOLD){
-				Sound.beep();
 				
+				boolean increaseX = false, increaseY = false;
 				double xDistanceFromGrid, yDistanceFromGrid;
-				
 				double	x = odometer.getX(),
 						y = odometer.getY();
 				
 				if(x>0){
 					xDistanceFromGrid = x%SQUARE_SIZE;
-					if(xDistanceFromGrid > SQUARE_SIZE/2)
+					if(xDistanceFromGrid > SQUARE_SIZE/2){
 						xDistanceFromGrid = SQUARE_SIZE - xDistanceFromGrid;
+						increaseX = true;
+					}
 				}
-				else
+				else{
 					xDistanceFromGrid = -x;
+				}
 				
 				if(y>0){
 					yDistanceFromGrid = y%SQUARE_SIZE;
-					if(yDistanceFromGrid > SQUARE_SIZE/2)
+					if(yDistanceFromGrid > SQUARE_SIZE/2){
 						yDistanceFromGrid = SQUARE_SIZE - yDistanceFromGrid;
+						increaseY = true;
+					}
 				}
-				else
-					yDistanceFromGrid = -y;
-				
-				if(xDistanceFromGrid < CORRECTION_THRESHOLD){
-					if(!(yDistanceFromGrid < CORRECTION_THRESHOLD))
-						if(x>0)
-							odometer.setPosition(new double[] {Math.floor(x/SQUARE_SIZE)*SQUARE_SIZE, 0, 0},
-												new boolean[] {true, false, false});
-						else
-							odometer.setPosition(new double[] {0, 0, 0}, new boolean[] {true, false, false});
-				}
-				else if(yDistanceFromGrid < CORRECTION_THRESHOLD)
-					if(y>0)
-						odometer.setPosition(new double[] {0, Math.floor(y/SQUARE_SIZE)*SQUARE_SIZE, 0},
-								new boolean[] {false, true, false});
-					else
-						odometer.setPosition(new double[] {0, 0, 0}, new boolean[] {false, true, false});
-				
 				else{
+					yDistanceFromGrid = -y;
+				}
+				
+				//For debugging purposes, indicates where correction may occur
+				if (xDistanceFromGrid < CORRECTION_THRESHOLD || yDistanceFromGrid < CORRECTION_THRESHOLD){
+					Sound.beep();
+				}
+				
+				if(xDistanceFromGrid < CORRECTION_THRESHOLD && !(yDistanceFromGrid < CORRECTION_THRESHOLD)){
+						if(x>0){
+							if (!increaseX){
+								odometer.setPosition(new double[] {Math.floor(x/SQUARE_SIZE)*SQUARE_SIZE, 0, 0},
+												new boolean[] {true, false, false});
+							}
+							else{
+								odometer.setPosition(new double[] {Math.ceil(x/SQUARE_SIZE)*SQUARE_SIZE, 0, 0},
+										new boolean[] {true, false, false});
+							}
+						}
+						else{
+							odometer.setPosition(new double[] {0, 0, 0}, new boolean[] {true, false, false});
+						}
+				}
+				else if(yDistanceFromGrid < CORRECTION_THRESHOLD && !(xDistanceFromGrid < CORRECTION_THRESHOLD)){
+					if(y>0){
+						if (!increaseY){
+							odometer.setPosition(new double[] {0, Math.floor(y/SQUARE_SIZE)*SQUARE_SIZE, 0},
+								new boolean[] {false, true, false});
+						}
+						else{
+							odometer.setPosition(new double[] {0, Math.ceil(y/SQUARE_SIZE)*SQUARE_SIZE, 0},
+									new boolean[] {false, true, false});
+						}
+					}
+					else{
+						odometer.setPosition(new double[] {0, 0, 0}, new boolean[] {false, true, false});
+					}
+				}
+				//For debugging purposes, indicates where correction has not occurred due to line crossing
+				else if (yDistanceFromGrid < CORRECTION_THRESHOLD && xDistanceFromGrid < CORRECTION_THRESHOLD){
 					Sound.buzz();
 				}
-					
 			}
-				
-			
 			try{Thread.sleep(POLLING_DELAY);}catch(InterruptedException e){}
 		}
 	}
